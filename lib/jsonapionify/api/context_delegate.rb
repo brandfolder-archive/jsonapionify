aa799398ab0c26f0176e45450f7ea70ac77f7b14
Jason Waldrip <jason@waldrip.net>
2015-12-11 14:21:22 -0700

    duping delegates

--- lib/jsonapionify/api/context_delegate.rb
+++ lib/jsonapionify/api/context_delegate.rb
@@ -11,6 +11,14 @@ module JSONAPIonify::Api
         request
       end
 
+      %i{initialize_dup initialize_clone}.each do |method|
+        define_singleton_method method do |copy|
+          memo.each do |k, v|
+            copy.public_send "#{k}=", v
+          end
+        end
+      end
+
       definitions.each do |name, context|
         define_singleton_method name do
           memo[name] ||= context.call(instance, delegate)
