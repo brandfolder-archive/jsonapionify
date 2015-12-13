f4d98c93efaabffbb96df6654fabf78eb872b3eb
Jason Waldrip <jason@waldrip.net>
2015-12-12 19:30:05 -0700

    use bundler setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
-require "active_support/json"
-require "codeclimate-test-reporter"
+require 'bundler/setup'
+require 'active_support/json'
+require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'jsonapionify'
 puts JSONAPIonify::Autoload.unloaded
 JSONAPIonify::Autoload.eager_load!
