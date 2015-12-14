f1e8ae39f4b183726ea1293770a3288169da5cfd
Jason Waldrip <jason@waldrip.net>
2015-12-14 13:21:38 -0700

    remove unused code

--- lib/jsonapionify/api/server.rb
+++ lib/jsonapionify/api/server.rb
@@ -27,106 +27,23 @@ module JSONAPIonify::Api
           request.env['jsonapionify.resource_name']                 = @resource if @resource
           request.env['jsonapionify.resource']                      = resource if @resource
           request.env['jsonapionify.id']                            = @id if @id
-          request.env['jsonapionify.relationship_request_type']     = @relationship == 'relationships' ? :full : :ids if @relationship
-          request.env['jsonapionify.relationship_name']             = @relationship == 'relationships' ? @relationship_name : @relationship if @relationship
-          request.env['jsonapionify.relationship']                  = @relationship = relationship if @relationship
         end
       end
 
       def response
-        resource.process(request)
-        # if resource && more.blank?
-        #   call_resource
-        # elsif resource.nil?
-        #   call_api_index
-        # else
-        #   Resource::Http.process(:not_found, request)
-        # end
+        @resource ? resource.process(request) : api_index
       rescue Errors::ResourceNotFound
         Resource::Http.process(:not_found, request)
       end
 
       private
 
-      def call_resource
-        id ? call_resource_with_id : call_collection
-      end
-
-      def call_resource_with_id
-        request.env['jsonapionify.id'] = id
-        return call_relationship if @relationship
-        if request.get?
-          resource.process(:read, request)
-        elsif request.put?
-          resource.process(:update, request)
-        elsif request.delete?
-          resource.process(:delete, request)
-        else
-          Resource::Http.process(:method_not_allowed, request)
-        end
-      end
-
-      def call_collection
-        if request.get?
-          resource.process(:index, request)
-        elsif request.post?
-          resource.process(:create, request)
-        else
-          Resource::Http.process(:method_not_allowed, request)
-        end
-      end
-
-      def call_relationship
-        if @relationship == 'relationships'
-          call_resource_relationship
-        else
-          call_resource_related
-        end
-      end
-
-      def call_resource_relationship
-        return call_not_found unless relationship
-        if request.get?
-          relationship.process(:show_relationship, request)
-        elsif request.patch?
-          relationship.process(:replace_relationship, request)
-        elsif request.post? && relationship.rel.is_a?(Relationship::Many)
-          relationship.process(:update_relationship, request)
-        elsif request.delete? && relationship.rel.is_a?(Relationship::Many)
-          relationship.process(:remove_relationship, request)
-        else
-          Resource::Http.process(:method_not_allowed, request)
-        end
-      end
-
-      def call_resource_related
-        return call_not_found unless relationship
-        if request.get? && relationship.rel.is_a?(Relationship::Many)
-          relationship.process(:index, request)
-        elsif request.get? && relationship.rel.is_a?(Relationship::One)
-          relationship.process(:read, request)
-        elsif request.post? && relationship.rel.is_a?(Relationship::Many)
-          relationship.process(:create, request)
-        else
-          Resource::Http.process(:method_not_allowed, request)
-        end
-      end
-
-      def call_api_index
+      def api_index
         api.process_index(request)
       end
 
-      def relationship
-        if @relationship == 'relationships'
-          raise Errors::RelationshipNotFound unless @relationship_name
-          resource.relationship(@relationship_name)
-        else
-          resource.relationship(@relationship)
-        end
-      end
-
       def resource
-        @resource && api.resource(@resource)
+        api.resource(@resource)
       end
     end
   end
