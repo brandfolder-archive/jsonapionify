ad641fc8e0a2251a5811d28cae552fd01bacc128
Jason Waldrip <jason@waldrip.net>
2015-12-17 00:06:26 -0700

    add documented

--- lib/jsonapionify/api/header_options.rb
+++ lib/jsonapionify/api/header_options.rb
@@ -1,12 +1,13 @@
 module JSONAPIonify::Api
   class HeaderOptions
 
-    attr_reader :name, :actions, :required
+    attr_reader :name, :actions, :required, :documented
 
-    def initialize(name, actions: nil, required: false)
-      @name     = name
-      @actions  = Array.wrap(actions)
-      @required = required
+    def initialize(name, actions: nil, required: false, documented: true)
+      @name       = name
+      @actions    = Array.wrap(actions)
+      @required   = required
+      @documented = documented
     end
 
   end
--- lib/jsonapionify/api/resource/definitions/request_headers.rb
+++ lib/jsonapionify/api/resource/definitions/request_headers.rb
@@ -8,58 +8,58 @@ module JSONAPIonify::Api
 
         # Standard HTTP Headers
         # https://en.wikipedia.org/wiki/List_of_HTTP_header_fields#Request_fields
-        request_header 'accept'
-        request_header 'accept-charset'
-        request_header 'accept-encoding'
-        request_header 'accept-language'
-        request_header 'accept-datetime'
-        request_header 'authorization'
-        request_header 'cache-control'
-        request_header 'connection'
-        request_header 'cookie'
-        request_header 'content-length'
-        request_header 'content-md5'
-        request_header 'content-type'
-        request_header 'date'
-        request_header 'expect'
-        request_header 'from'
-        request_header 'host'
-        request_header 'if-match'
-        request_header 'if-modified-since'
-        request_header 'if-none-match'
-        request_header 'if-range'
-        request_header 'if-unmodified-since'
-        request_header 'max-forwards'
-        request_header 'origin'
-        request_header 'pragma'
-        request_header 'proxy-authorization'
-        request_header 'range'
-        request_header 'referer'
-        request_header 'te'
-        request_header 'user-agent'
-        request_header 'upgrade'
-        request_header 'via'
-        request_header 'warning'
+        request_header 'accept', documented: false
+        request_header 'accept-charset', documented: false
+        request_header 'accept-encoding', documented: false
+        request_header 'accept-language', documented: false
+        request_header 'accept-datetime', documented: false
+        request_header 'authorization', documented: false
+        request_header 'cache-control', documented: false
+        request_header 'connection', documented: false
+        request_header 'cookie', documented: false
+        request_header 'content-length', documented: false
+        request_header 'content-md5', documented: false
+        request_header 'content-type', documented: false
+        request_header 'date', documented: false
+        request_header 'expect', documented: false
+        request_header 'from', documented: false
+        request_header 'host', documented: false
+        request_header 'if-match', documented: false
+        request_header 'if-modified-since', documented: false
+        request_header 'if-none-match', documented: false
+        request_header 'if-range', documented: false
+        request_header 'if-unmodified-since', documented: false
+        request_header 'max-forwards', documented: false
+        request_header 'origin', documented: false
+        request_header 'pragma', documented: false
+        request_header 'proxy-authorization', documented: false
+        request_header 'range', documented: false
+        request_header 'referer', documented: false
+        request_header 'te', documented: false
+        request_header 'user-agent', documented: false
+        request_header 'upgrade', documented: false
+        request_header 'via', documented: false
+        request_header 'warning', documented: false
 
         # Non-Standard, but widely used HTTP headers
-        request_header 'x-requested-with'
-        request_header 'dnt'
-        request_header 'x-forwarded-for'
-        request_header 'x-forwarded-host'
-        request_header 'x-forwarded-proto'
-        request_header 'front-end-https'
-        request_header 'x-att-device-id'
-        request_header 'x-wap-profile'
-        request_header 'proxy-connection'
-        request_header 'x-uidh'
-        request_header 'upgrade-insecure-requests'
+        request_header 'x-requested-with', documented: false
+        request_header 'dnt', documented: false
+        request_header 'x-forwarded-for', documented: false
+        request_header 'x-forwarded-host', documented: false
+        request_header 'x-forwarded-proto', documented: false
+        request_header 'front-end-https', documented: false
+        request_header 'x-att-device-id', documented: false
+        request_header 'x-wap-profile', documented: false
+        request_header 'proxy-connection', documented: false
+        request_header 'x-uidh', documented: false
+        request_header 'upgrade-insecure-requests', documented: false
 
         # Don't allow method overrides
-        # request_header 'x-http-method-override'
+        # request_header 'x-http-method-override', documented: false
 
         # Don't allow CSRF tokens, as they should not be used
         # in the api by default
-        # request_header 'x-csrf-token'
+        # request_header 'x-csrf-token', documented: false
 
         before do |context|
           context.request_headers # pull request_headers so they verify
