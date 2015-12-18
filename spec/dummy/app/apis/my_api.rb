ac507ab52ea5a5d8e6fe38cbdd658e3ff093b9eb
Jason Waldrip <jason@waldrip.net>
2015-12-18 13:21:09 -0700

    add links and fix top header

--- spec/dummy/app/apis/my_api.rb
+++ spec/dummy/app/apis/my_api.rb
@@ -3,6 +3,8 @@ class MyApi < JSONAPIonify::Api::Base
   title "My Awesome API"
   cache :memory_store
 
+  link 'Website', 'http://example.org'
+
   rescue_from ActiveRecord::RecordNotFound, error: :not_found
 
   documentation_order(
