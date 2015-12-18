89fcfb360be25a27e74c67eeffbb37946130e4f0
Jason Waldrip <jason@waldrip.net>
2015-12-18 15:49:36 -0700

    fix docs

--- lib/jsonapionify/api/base/doc_helper.rb
+++ lib/jsonapionify/api/base/doc_helper.rb
@@ -22,13 +22,15 @@ module JSONAPIonify::Api
     end
 
     def documentation_output(request)
-      #@documentation_output ||=
-          JSONAPIonify::Documentation.new(documentation_object(request)).result
+      @documentation_output ||=
+        JSONAPIonify::Documentation.new(documentation_object(request)).result
     end
 
     def resources_in_order
       indexes = @documentation_order || []
-      resources.sort_by { |resource| indexes.index(resource.name) || indexes.length }
+      resources.sort_by do |resource|
+        indexes.map(&:to_s).index(resource.type) || indexes.length
+      end
     end
 
     def documentation_object(request)
@@ -38,11 +40,11 @@ module JSONAPIonify::Api
         uri.path.chomp! '/docs'
       end.to_s
       OpenStruct.new(
-          links: links,
-          title: @title || self.name,
-          base_url: base_url,
-          description: JSONAPIonify::Documentation.render_markdown(@description || ''),
-          resources: resources_in_order.map { |r| r.documentation_object base_url }
+        links:       links,
+        title:       @title || self.name,
+        base_url:    base_url,
+        description: JSONAPIonify::Documentation.render_markdown(@description || ''),
+        resources:   resources_in_order.map { |r| r.documentation_object base_url }
       )
     end
   end
