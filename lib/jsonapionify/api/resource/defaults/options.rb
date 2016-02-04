686b0c587a668a2d17728df93c14ebcaa985ebfd
Jason Waldrip <jason@waldrip.net>
2016-02-04 13:41:37 -0700

    add option checkers

--- lib/jsonapionify/api/resource/defaults/options.rb
+++ lib/jsonapionify/api/resource/defaults/options.rb
@@ -8,6 +8,38 @@ module JSONAPIonify::Api
       instance { raise NotImplementedError, 'instance not implemented' }
       new_instance { raise NotImplementedError, 'new instance not implemented' }
 
+      context :scope_defined? do |context|
+        begin
+          !!context.scope
+        rescue NotImplementedError
+          false
+        end
+      end
+
+      context :collection_defined? do |context|
+        begin
+          !!context.collection
+        rescue NotImplementedError
+          false
+        end
+      end
+
+      context :instance_defined? do |context|
+        begin
+          !!context.instance
+        rescue NotImplementedError
+          false
+        end
+      end
+
+      context :new_instance_defined? do |context|
+        begin
+          !!context.new_instance
+        rescue NotImplementedError
+          false
+        end
+      end
+
       before do |context|
         context.request_headers # pull request_headers so they verify
       end
