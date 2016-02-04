d0f85706a667670c65d12b6ef225d441fdf3ef03
Jason Waldrip <jason@waldrip.net>
2016-02-03 21:19:42 -0700

    ensure create is on a single relationship

--- lib/jsonapionify/api/relationship/one.rb
+++ lib/jsonapionify/api/relationship/one.rb
@@ -3,9 +3,10 @@ module JSONAPIonify::Api
 
     prepend_class do
       rel = self.rel
-      remove_action :list, :create
+      remove_action :list
+
       class << self
-        undef_method :list, :create
+        undef_method :list
       end
 
       define_singleton_method(:show) do |**options, &block|
