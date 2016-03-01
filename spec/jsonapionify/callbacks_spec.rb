5ba626a02058065ba323ce3b60016870f60cadce
Jason Waldrip <jason@waldrip.net>
2016-03-01 16:56:23 -0700

    test the order of callbacks

--- spec/jsonapionify/callbacks_spec.rb
+++ spec/jsonapionify/callbacks_spec.rb
@@ -19,5 +19,27 @@ module JSONAPIonify
       end
     end
 
+    it 'should return the value of the block' do
+      before_value = false
+      in_value = false
+      after_value = false
+
+      klass.after_default do
+        after_value = before_value && in_value
+      end
+
+      klass.before_default do
+        before_value = true
+      end
+
+      value = klass.new.run_callbacks(:default) do
+        in_value = before_value
+        "FOO"
+      end
+
+      expect(after_value).to be_truthy
+      expect(value).to eq "FOO"
+    end
+
   end
 end
