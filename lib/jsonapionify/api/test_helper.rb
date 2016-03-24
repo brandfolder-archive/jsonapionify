00bacc0accdb3fae8e9e634b58181159daed75ed
Jason Waldrip <jason@waldrip.net>
2016-03-24 17:11:59 -0600

    much better response error messages

--- lib/jsonapionify/api/test_helper.rb
+++ lib/jsonapionify/api/test_helper.rb
@@ -6,12 +6,54 @@ module JSONAPIonify
     extend ActiveSupport::Concern
     include Rack::Test::Methods
 
+    included do
+      around(:each) do |example|
+        if example.run && last_response && (last_response_json rescue false)
+          aggregate_failures do
+            "response failures"
+            last_response_error_messages.each do |message|
+              expect(message).to be_empty, message
+            end
+          end
+        end
+      end
+    end
+
     module ClassMethods
       def set_api(api)
         define_method(:app) do
           api
         end
       end
+    end
+
+    def last_response_error_messages
+      last_response_errors.map do |error|
+        {
+          id:        error['id'],
+          code:      error['code'],
+          status:    error['status'],
+          title:     error['title'],
+          detail:    error['detail'],
+          links:     error['links'],
+          source:    error['source']&.each_with_object([]) { |(k, v), a|
+            a << "\n  #{k}: #{v}" if v
+          }&.join,
+          meta:      error['meta']&.except('backtrace')&.each_with_object([]) { |(k, v), a|
+            a << "\n  #{k}: #{v}" if v
+          }&.join,
+          ': ':       ':',
+          backtrace: ["\n", *(error.dig('meta', 'backtrace') || [])].join("\n")
+        }.each_with_object([]) { |(k, v), a|
+          a << "#{k}: #{v}" if v&.strip
+        }.join("\n")
+      end
+    end
+
+    def last_response_errors
+      last_response_json['errors']
+    rescue
+      []
     end
 
     def set_headers
