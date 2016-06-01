a27178196de589b7bc7f8b0b1eba7552be937f7d
jwaldrip <jason@waldrip.net>
2016-05-31 23:23:07 -0600

    allow the count attribute to be hidden

--- lib/jsonapionify/api/resource/definitions/relationships.rb
+++ lib/jsonapionify/api/resource/definitions/relationships.rb
@@ -8,11 +8,12 @@ module JSONAPIonify::Api
       end
     end
 
-    def relates_to_many(name, count_attribute: false, include_count: true, **opts, &block)
+    def relates_to_many(name, count_attribute: false, count_hidden: false, **opts, &block)
       define_relationship(name, Relationship::Many, **opts, &block).tap do
         define_relationship_counter(
           name,
-          count_attribute === true ? "#{name.to_s.singularize}_count" : count_attribute.to_s
+          count_attribute === true ? "#{name.to_s.singularize}_count" : count_attribute.to_s,
+          count_hidden
         ) if count_attribute
       end
     end
@@ -22,8 +23,8 @@ module JSONAPIonify::Api
       define_relationship(name, Relationship::One, **opts, &block)
     end
 
-    def define_relationship_counter(rel_name, name, include: true)
-      attribute name.to_sym, types.Integer, "The number of #{rel_name}.", write: false do |_, instance, context|
+    def define_relationship_counter(rel_name, name, hidden = false)
+      attribute name.to_sym, types.Integer, "The number of #{rel_name}.", hidden: hidden, write: false do |_, instance, context|
         rel          = context.resource.class.relationship(rel_name)
         rel_context  = rel.new(
           request:           context.request,
