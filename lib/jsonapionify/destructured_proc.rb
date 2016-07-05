68196c999304bcae7efba4904a3f240418fbca47
jwaldrip <jason@waldrip.net>
2016-07-05 10:43:00 -0600

    only destructure if kwargs are passed

--- lib/jsonapionify/destructured_proc.rb
+++ lib/jsonapionify/destructured_proc.rb
@@ -1,22 +1,24 @@
 module JSONAPIonify::DestructuredProc
   refine Proc do
     def destructure(at_index = arguments.length-1)
+      return self unless kwargs.present?
       original = self
       Proc.new do |*args|
-        kwargs = original.kwargs(args[at_index])
-        begin
-          instance_exec(*args, **kwargs, &original)
-        end
+        kwargs = original.kwargs_destructured(args[at_index])
+        instance_exec(*args, **kwargs, &original)
       end
+    end
+
+    def kwargs
+      parameters.select { |t, k| t.to_s.start_with? 'key' }
     end
 
     def arguments
       parameters.reject { |t, k| t.to_s.start_with? 'key' }
     end
 
-    def kwargs(target)
-      parameters.each_with_object({}) do |(type, key), kw|
-        next unless type == :key || type == :keyreq
+    def kwargs_destructured(target)
+      kwargs.each_with_object({}) do |(_, key), kw|
         next unless target.respond_to? key
         kw[key] = target.public_send(key)
       end
