4b3afe14d8e8d3b639a4450adde1aeb5d33f66c0
jwaldrip <jason@waldrip.net>
2016-07-20 09:48:16 -0600

    consolidate callbacks

--- lib/jsonapionify/api/resource/callbacks.rb
+++ lib/jsonapionify/api/resource/callbacks.rb
@@ -5,56 +5,11 @@ module JSONAPIonify::Api
     extend ActiveSupport::Concern
     using JSONAPIonify::DestructuredProc
     included do
+      include JSONAPIonify::Callbacks
 
-      def self.define_callbacks(*names)
-        names.each do |name|
-          chains = {
-            main:   "__#{name}_callback_chain",
-            before: "__#{name}_before_callback_chain",
-            after:  "__#{name}_after_callback_chain"
-          }
-          define_method chains[:main] do |*args, &block|
-            block ||= proc {}
-            if send(chains[:before], *args) != false
-              value = begin
-                instance_exec(*args, @__context, &block&.destructure)
-              rescue => e
-                e.backtrace.unshift block.source_location.join(':') + ":in `(callback)`"
-                raise e
-              end
-              value if send(chains[:after], *args) != false
-            end
-          end unless method_defined? chains[:main]
-
-          # Define before and after chains
-          %i{after before}.each do |timing|
-            define_method chains[timing] { |*| } unless method_defined? chains[timing]
-            callback_name = "#{timing}_#{name}"
-            define_singleton_method callback_name do |sym = nil, &outer_block|
-              outer_block = (outer_block || sym).to_proc
-              prev_chain  = instance_method(chains[timing])
-              define_method chains[timing] do |*args, &block|
-                begin
-                  if prev_chain.bind(self).call(*args, @__context, &block&.destructure) != false
-                    instance_exec(*args, @__context, &outer_block&.destructure)
-                  end
-                rescue => e
-                  e.backtrace.unshift outer_block.source_location.join(':') + ":in `(callback) #{callback_name}`"
-                  raise e
-                end
-              end
-            end
-          end
-
-          private(*chains.values)
-
-        end
+      define_callback_strategy do |*args, &block|
+        instance_exec(*args, @__context, &block.destructure)
       end
     end
-
-    def run_callbacks(name, *args, &block)
-      send("__#{name}_callback_chain", *args, &block)
-    end
-
   end
 end
--- lib/jsonapionify/callbacks.rb
+++ lib/jsonapionify/callbacks.rb
@@ -1,11 +1,16 @@
 require 'active_support/concern'
+require 'unstrict_proc'
 
 module JSONAPIonify
   module Callbacks
     extend ActiveSupport::Concern
-    included do
 
-      def self.define_callbacks(*names)
+    module ClassMethods
+      def define_callback_strategy(&strategy)
+        define_method(:__callback_strategy, &strategy)
+      end
+
+      def define_callbacks(*names)
         names.each do |name|
           chains = {
             main:   "__#{name}_callback_chain",
@@ -14,10 +19,12 @@ module JSONAPIonify
           }
           define_method chains[:main] do |*args, **, &block|
             block ||= proc {}
-            if send(chains[:before], *args) != false
-              value = instance_exec(*args, &block)
-              value if send(chains[:after], *args) != false
-            end
+            false != send(chains[:before], *args) &&
+              (value = JSONAPIonify::CustomRescue.perform(remove: __FILE__, source: block, formatter: ->(meta) { meta.source_location.join(':') + ":in callback: `run'" }) do
+                __callback_strategy(*args, &block) || true
+              end) &&
+              false != send(chains[:after], *args) &&
+              value
           end unless method_defined? chains[:main]
 
           # Define before and after chains
@@ -27,13 +34,11 @@ module JSONAPIonify
             define_singleton_method callback_name do |sym = nil, &outer_block|
               outer_block = (outer_block || sym).to_proc
               prev_chain  = instance_method(chains[timing])
-              define_method chains[timing] do |*args, **, &block|
-                begin
-                  instance_exec(*args, &outer_block) if prev_chain.bind(self).call(*args, &block) != false
-                rescue => e
-                  e.backtrace.unshift outer_block.source_location.join(':') + ":in `(callback) #{callback_name}`"
-                  raise e
-                end
+              define_method chains[timing] do |*args, &block|
+                false != prev_chain.bind(self).call(*args, &block) &&
+                  JSONAPIonify::CustomRescue.perform(remove: __FILE__, source: outer_block, formatter: ->(meta) { meta.source_location.join(':') + ":in callback: `#{timing}_#{name}'" }) do
+                    __callback_strategy(*args, &outer_block)
+                  end
               end
             end
           end
@@ -41,6 +46,12 @@ module JSONAPIonify
           private(*chains.values)
 
         end
+      end
+    end
+
+    included do
+      define_callback_strategy do |*args, &block|
+        instance_exec(*args, &block)
       end
     end
 
